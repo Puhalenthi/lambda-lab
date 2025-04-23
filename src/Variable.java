8d6c8ce24c8390b45595af2e0f2494fc1891326e
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2025-04-23 14:58:24 -0400

    add initial code

---
+++ src/Variable.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Variable implements Expression {
+	private String name;
+	
+	public Variable(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+
+}
