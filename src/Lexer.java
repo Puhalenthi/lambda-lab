5ffd8dfdc1562c998373d60f21114c32acd987fc
ZeviCohen <123010327+Zevirino@users.noreply.github.com>
2025-04-23 21:17:43 -0400

    create lexer (step 3)

--- src/Lexer.java
+++ src/Lexer.java
@@ -5,12 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
-	
+
 	/*
 	 * A lexer (or "tokenizer") converts an input into tokens that
 	 * eventually need to be interpreted.
-	 * 
-	 * Given the input 
+	 *
+	 * Given the input
 	 *    (\bat  .bat flies)cat  λg.joy! )
 	 * you should output the ArrayList of strings
 	 *    [(, \, bat, ., bat, flies, ), cat, \, g, ., joy!, )]
@@ -19,8 +19,34 @@ public class Lexer {
 	public ArrayList<String> tokenize(String input) {
 		ArrayList<String> tokens = new ArrayList<String>();
 
-		// This next line is definitely incorrect!
-		tokens.add(input);
+		int index = 0;
+		String variableName = "";
+		char currentChar = input.charAt(index);
+
+		while (currentChar != ';' && index<input.length()){
+			if (currentChar=='(' || currentChar==')' || currentChar=='\\' || currentChar=='.' || currentChar=='=' || currentChar==' '){
+				if (!variableName.equals("")){
+					tokens.add(variableName);
+					variableName = "";
+				}
+				if (currentChar!=' '){
+					tokens.add(currentChar+"");
+				}
+			} else {
+				variableName += currentChar;
+			}
+
+			index++;
+			if (index!=input.length()){
+				currentChar = input.charAt(index);
+			}
+		}
+
+		if (index==input.length()){
+			if (!variableName.equals("")){
+				tokens.add(variableName);
+			}
+		}
 
 		return tokens;
 	}
